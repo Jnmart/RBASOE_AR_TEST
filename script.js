ff2effa861eac6320ba99ba3267c8b517aa57ce7
Jnmart <martinezjy@uindy.edu>
2021-04-22 21:59:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function screenShot()
-{
-var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+            function draw(){
+                var canvas = document.getElementById("thecanvas");
+                var ctx = canvas.getContext("2d");
+                ctx.fillStyle = "rgba(125, 46, 138, 0.5)";
+                ctx.fillRect(25,25,100,100); 
+                ctx.fillStyle = "rgba( 0, 146, 38, 0.5)";
+                ctx.fillRect(58, 74, 125, 100);
+            }
 
-window.location.href=image; // it will save locally                     
-}  
+            function screenShot(){
+                var canvas = document.getElementById("thecanvas");
+                document.getElementById("theimage").src = canvas.toDataURL();
+                Canvas2Image.saveAsPNG(canvas);
+            }
